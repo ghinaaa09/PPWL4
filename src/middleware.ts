30c6fdda4b945471a19f7a633777f1f53bd1b753
Ghina <h1101241015@student.untan.ac.id>
2026-03-03 21:30:32 +0700

    Praktikum 1 - Validasi Request Body

---
+++ src/middleware.ts
@@ -0,0 +1,21 @@
+import { Elysia, t } from "elysia"
+
+const app = new Elysia()
+
+// Global Logger
+app.onRequest(({ request }) => {
+  console.log("📥", request.method, request.url)
+  console.log("🕒", new Date().toISOString())
+})
+
+app.onRequest(({ request, set }) => {
+  if (request.headers.get("x-block") === "true") {
+    set.status = 403
+    return { message: "Blocked" }
+  }
+})
+
+app.get("/", () => "Hello Middleware")
+
+app.listen(3000)
+console.log("Server running at http://localhost:3000") 
