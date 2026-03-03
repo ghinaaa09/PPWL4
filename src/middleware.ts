635017a0ab928d719df9ecd9c2276ddd0b2c57f2
Ghina <h1101241015@student.untan.ac.id>
2026-03-03 23:09:48 +0700

    Praktikum 4 - beforeHandle

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,41 +1,58 @@
-import { Elysia } from "elysia"
-import { openapi } from "@elysiajs/openapi";
+    import { Elysia } from "elysia"
+    import { openapi } from "@elysiajs/openapi";
 
-const app = new Elysia()
-  .use(openapi())
+    const app = new Elysia()
+      .use(openapi())
 
-// GLOBAL LOGGER (onRequest)
-app.onRequest(({ request }) => {
-  console.log("📥", request.method, request.url)
-  console.log("🕒", new Date().toISOString())
-})
+    // ==========================
+    // GLOBAL LOGGER
+    // ==========================
+    app.onRequest(({ request }) => {
+      console.log("📥", request.method, request.url)
+      console.log("🕒", new Date().toISOString())
+    })
 
-// SHORT-CIRCUIT
-app.onRequest(({ request, set }) => {
-  if (request.headers.get("x-block") === "true") {
-    set.status = 403
-    return { message: "Blocked by middleware" }
-  }
-})
+    // ==========================
+    // SHORT-CIRCUIT
+    // ==========================
+    app.onRequest(({ request, set }) => {
+      if (request.headers.get("x-block") === "true") {
+        set.status = 403
+        return { message: "Blocked by middleware" }
+      }
+    })
 
-// BEFORE HANDLE (Auth Guard)
-app.get(
-  "/dashboard",
-  () => ({
-    message: "Welcome to Dashboard"
-  }),
-  {
-    beforeHandle({ headers, set }) {
-      if (!headers.authorization) {
-        set.status = 401
-        return {
-          success: false,
-          message: "Unauthorized"
+    // ==========================
+    // BEFORE HANDLE - /admin
+    // ==========================
+    app.get(
+      "/admin",
+      () => ({
+        stats: 99
+      }),
+      {
+        beforeHandle({ headers, set }) {
+          if (headers.authorization !== "Bearer 123") {
+            set.status = 401
+            return {
+              success: false,
+              message: "Unauthorized"
+            }
+          }
         }
       }
-    }
-  }
-)
+    )
 
-app.listen(3000)
-console.log("Server running at http://localhost:3000")
+    // ==========================
+    // AFTER HANDLE
+    // ==========================
+    app.onAfterHandle(({ response }) => {
+    return {
+      success: true,
+      data: response
+    }
+  })
+
+    app.listen(3000)
+
+    console.log("Server running at http://localhost:3000")
